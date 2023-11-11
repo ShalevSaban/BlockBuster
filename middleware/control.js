9cdefb87f003dca52e23e974cf8140c6efe826bd
ShalevSaban <shalevsaban1@gmail.com>
2023-11-11 14:49:08 +0200

    Update control file

--- middleware/control.js
+++ middleware/control.js
@@ -1,19 +1,19 @@
 function controlFunction(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  const allowedOrigins = [
-    "http://localhost:5173",
-    "https://bluckbuster.onrender.com",
-  ];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.header("Access-Control-Allow-credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+  // const allowedOrigins = [
+  //   "http://localhost:5173",
+  //   "https://bluckbuster.onrender.com",
+  // ];
+  // const origin = req.headers.origin;
+  // if (allowedOrigins.includes(origin)) {
+  //   res.setHeader("Access-Control-Allow-Origin", origin);
+  // }
+  // res.header(
+  //   "Access-Control-Allow-Headers",
+  //   "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  // );
+  // res.header("Access-Control-Allow-credentials", true);
+  // res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
   next();
 }
 
